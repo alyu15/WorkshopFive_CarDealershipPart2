0309e0dee577a2fb130fd6898090841e9f093010
Alisha Yu <alisha.yu15@gmail.com>
2024-05-16 15:54:45 -0400

    added more variation to dealership inventory

--- src/main/java/com/ps/ContractDataManager.java
+++ src/main/java/com/ps/ContractDataManager.java
@@ -1,7 +1,29 @@
 package com.ps;
 
-public class ContractDataManager {
-    public static void saveContract(){
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class ContractDataManager {
+
+    public static void saveContract(Contract contract){
+
+        try {
+            BufferedWriter buffWriter = new BufferedWriter(new FileWriter("Contracts.csv",true));
+
+            if(contract instanceof SalesContract) {
+            // SALE|phone number|Name|email|vin|year|make|model|type|color|mileage|price|
+                // sales tax price|recording fee|processing fee|total price|monthly payments y/n|monthly payment amount
+
+
+
+            } else {
+            // LEASE|phone number|Name|email|vin|year|make|model|type|color|mileage|price|
+                // ending value|lease fee|total price|monthly payment
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
